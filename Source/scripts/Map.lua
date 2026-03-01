58d811088976b165f7ec25b9107bc308c2734311
Christever <christian@steverlynck.fr>
2026-03-01 12:55:52 +0100

    Second commit

---
+++ Source/scripts/Map.lua
@@ -0,0 +1,71 @@
+local Map = {}
+
+
+
+---Initialisation de la map
+---@param pLevel number (Niveau)
+function Map.init(pLevel)
+    local level = require("maps.levels")
+    Map.level = level[pLevel]
+    Map.width    = #Map.level[1]
+    Map.height   = #Map.level
+
+    Map.explored = {}
+    for y = 1, Map.height do
+        Map.explored[y] = {}
+        for x = 1, Map.width do
+            Map.explored[y][x] = false
+        end
+    end
+end
+
+function Map.draw()
+    local oldFont = love.graphics.getFont()
+    local r, g, b, a = love.graphics.getColor()
+
+    love.graphics.setFont(font_xl)
+    love.graphics.setColor(Color.WHITE)
+    for y, line in ipairs(Map.level) do
+        for x = 1, #line do
+            local char = line:sub(x, x)
+            if char == "#" then love.graphics.setColor(Color.WHITE) end
+            if char == "F" then love.graphics.setColor(Color.GREEN) end
+            if char == "C" then love.graphics.setColor(Color.WHITE) end
+            if char == "W" then love.graphics.setColor(Color.BLUE) end
+            if char == "M" then love.graphics.setColor(Color.GRAY) end
+            love.graphics.print(char, (x - 1) * TILESIZE, (y - 1) * TILESIZE)
+        end
+    end
+
+    love.graphics.setColor(Color.DARKBLACK)
+    for y = 1, Map.height do
+        for x = 1, Map.width do
+            if not isVisible(x, y, player.x, player.y, 3) then
+                if Map.explored[y][x] then
+                    love.graphics.setColor(Color.MEDIUMBLACK)
+                else
+                    love.graphics.setColor(Color.BLACK)
+                end
+                love.graphics.rectangle("fill", (x - 1) * TILESIZE, (y - 1) * TILESIZE, TILESIZE, TILESIZE)
+            end
+        end
+    end
+    love.graphics.setFont(oldFont)
+    love.graphics.setColor(r, g, b, a)
+end
+
+function Map.getTile(pX, pY)
+    return Map.level[pY]:sub(pX, pX)
+end
+
+function Map.setExplored()
+    for y = 1, Map.height do
+        for x = 1, Map.width do
+            if isVisible(x, y) then
+                Map.explored[y][x] = true
+            end
+        end
+    end
+end
+
+return Map
---
+++ Source/scripts/Player.lua
@@ -0,0 +1,36 @@
+local Player = {}
+
+
+function Player.init()
+    Player.x = 2
+    Player.y = 2
+    Player.force = 40
+    Player.attack = 10
+    Player.armure = 15
+    Player.const = 80
+    Player.gold = 0
+end
+
+function Player.draw()
+    local oldFont = love.graphics.getFont()
+    love.graphics.setFont(font_xl)
+    love.graphics.setColor(Color.DARKGREEN)
+    love.graphics.print("P", (Player.x - 1) * TILESIZE, (Player.y - 1) * TILESIZE)
+    love.graphics.setFont(oldFont)
+end
+
+function Player.keypressed(key)
+    local newX, newY = Player.x, Player.y
+    if key == "up" then
+        newY = newY - 1
+    elseif key == "down" then
+        newY = newY + 1
+    elseif key == "left" then
+        newX = newX - 1
+    elseif key == "right" then
+        newX = newX + 1
+    end
+    return newX, newY
+end
+
+return Player
